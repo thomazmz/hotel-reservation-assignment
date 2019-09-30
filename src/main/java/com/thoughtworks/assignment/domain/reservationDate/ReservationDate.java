f63ba25c7182781a667cabb15af2c1927fb0047c
thomaz <thomaz.zandonotto@gmail.com>
2019-09-30 20:55:28 -0300

    Rename date wrapper model

---
+++ src/main/java/com/thoughtworks/assignment/domain/reservationDate/ReservationDate.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.assignment.domain.stayDate;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class ReservationDate {
+
+    private LocalDate date;
+
+    public ReservationDate(Integer year, Month month, Integer day) {
+        this.date = LocalDate.of(year, month, day);
+    }
+
+    public ReservationDateClassification getClassification() {
+        return ReservationDateClassification.getClassification(this.date);
+    }
+}
--- src/main/java/com/thoughtworks/assignment/domain/reservationDate/ReservationDateClassification.java
+++ src/main/java/com/thoughtworks/assignment/domain/reservationDate/ReservationDateClassification.java
@@ -3,11 +3,11 @@ package com.thoughtworks.assignment.domain.stayDate;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
-public enum StayDateClassification {
+public enum ReservationDateClassification {
     WEEK_DAY,
     WEEKEND_DAY;
 
-    public static StayDateClassification getClassification(LocalDate date) {
+    public static ReservationDateClassification getClassification(LocalDate date) {
         if(DayOfWeek.from(date) == DayOfWeek.SATURDAY || DayOfWeek.from(date) == DayOfWeek.SUNDAY) {
             return WEEKEND_DAY;
         }
