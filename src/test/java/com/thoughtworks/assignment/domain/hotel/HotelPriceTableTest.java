9cfb91898ef919ec4e8d9cd34cc3e883458ca124
thomaz <thomaz.zandonotto@gmail.com>
2019-09-30 22:55:10 -0300

    Test refectoring

--- src/test/java/com/thoughtworks/assignment/domain/hotel/HotelPriceTableTest.java
+++ src/test/java/com/thoughtworks/assignment/domain/hotel/HotelPriceTableTest.java
@@ -1,35 +1,58 @@
 package com.thoughtworks.assignment.domain.hotel;
 
 import com.thoughtworks.assignment.domain.client.ClientClassification;
+import com.thoughtworks.assignment.domain.reservationDate.ReservationDate;
 import com.thoughtworks.assignment.domain.reservationDate.ReservationDateClassification;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HotelPriceTableTest {
 
+    HotelPriceTable priceTable;
+
+    @BeforeEach
+    void createNewHotel() {
+        // Given
+        priceTable = new HotelPriceTable();
+        priceTable.addPrice(ReservationDateClassification.WEEK_DAY, ClientClassification.REGULAR, 50D);
+        priceTable.addPrice(ReservationDateClassification.WEEK_DAY, ClientClassification.REWARD, 40D);
+        priceTable.addPrice( ReservationDateClassification.WEEKEND_DAY, ClientClassification.REGULAR, 80D);
+    }
+
     @Test
     void shouldAddPriceToPriceTable() {
         // Given
-        HotelPriceTable priceTable = new HotelPriceTable();
-        priceTable.addPrice(ReservationDateClassification.WEEK_DAY, ClientClassification.REGULAR, 50D);
+        priceTable.addPrice(ReservationDateClassification.WEEKEND_DAY, ClientClassification.REWARD, 70D);
         // When
         Double price = priceTable.getPrice(ReservationDateClassification.WEEK_DAY, ClientClassification.REGULAR);
         // Then
-        assertEquals(priceTable.size(), 1);
-        assertEquals(price, 50D);
+        assertEquals(4, priceTable.size());
+        assertEquals(50D, price);
     }
 
     @Test
     void shouldRemovePriceFromPriceTable() {
-        // Given
-        HotelPriceTable priceTable = new HotelPriceTable();
-        priceTable.addPrice(ReservationDateClassification.WEEK_DAY, ClientClassification.REGULAR, 50D);
         // When
         priceTable.removePrice(ReservationDateClassification.WEEK_DAY, ClientClassification.REGULAR);
         // Then
         assertNull(priceTable.getPrice(ReservationDateClassification.WEEK_DAY, ClientClassification.REGULAR));
-        assertEquals(priceTable.size(), 0);
+        assertEquals(2, priceTable.size());
+    }
+
+    @Test
+    void shouldReturnPriceGivenDateAndClientTye() {
+        // Given
+        ReservationDate date = new ReservationDate(2019, Month.SEPTEMBER, 27);
+        // When
+        Double regularPrice = priceTable.getPrice(date, ClientClassification.REGULAR);
+        Double rewardPrice = priceTable.getPrice(date, ClientClassification.REWARD);
+        // Then
+        assertEquals(50D, regularPrice);
+        assertEquals(40D, rewardPrice);
     }
 
 }
--- src/test/java/com/thoughtworks/assignment/domain/reservationDate/ReservationDateClassificationTest.java
+++ src/test/java/com/thoughtworks/assignment/domain/reservationDate/ReservationDateClassificationTest.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.assignment.domain.reservationDate;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -9,40 +12,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationDateClassificationTest {
 
-    @Test
-    void shouldReturnProperDateClassificationForWeekDays() {
+    @ParameterizedTest()
+    @CsvSource({
+        "2019, SEPTEMBER, 23",
+        "2019, SEPTEMBER, 24",
+        "2019, SEPTEMBER, 25",
+        "2019, SEPTEMBER, 26",
+        "2019, SEPTEMBER, 27"
+    })
+    void shouldReturnProperDateClassificationForWeekDays(int year, String month, int day) {
         // Given
-        LocalDate mondayDate = LocalDate.of(2019, Month.SEPTEMBER, 23);
-        LocalDate tuesdayDate = LocalDate.of(2019, Month.SEPTEMBER, 24);
-        LocalDate wednesdayDate = LocalDate.of(2019, Month.SEPTEMBER, 25);
-        LocalDate thursdayDate = LocalDate.of(2019, Month.SEPTEMBER, 26);
-        LocalDate fridayDate = LocalDate.of(2019, Month.SEPTEMBER, 27);
-
+        LocalDate date = LocalDate.of(year, Month.valueOf(month), day);
         // When
-        ReservationDateClassification mondayDateClassification = ReservationDateClassification.getClassification(mondayDate);
-        ReservationDateClassification tuesdayDateClassification = ReservationDateClassification.getClassification(tuesdayDate);
-        ReservationDateClassification wednesdayDateClassification = ReservationDateClassification.getClassification(wednesdayDate);
-        ReservationDateClassification thursdayDateClassification = ReservationDateClassification.getClassification(thursdayDate);
-        ReservationDateClassification fridayDateClassification = ReservationDateClassification.getClassification(fridayDate);
-
+        ReservationDateClassification dateClassification = ReservationDateClassification.getClassification(date);
         // Then
-        assertEquals(mondayDateClassification, ReservationDateClassification.WEEK_DAY);
-        assertEquals(tuesdayDateClassification, ReservationDateClassification.WEEK_DAY);
-        assertEquals(wednesdayDateClassification, ReservationDateClassification.WEEK_DAY);
-        assertEquals(thursdayDateClassification, ReservationDateClassification.WEEK_DAY);
-        assertEquals(fridayDateClassification, ReservationDateClassification.WEEK_DAY);
+        assertEquals(ReservationDateClassification.WEEK_DAY, dateClassification);
     }
 
-    @Test
-    void shouldReturnProperDateClassificationForWeekendDays() {
+    @ParameterizedTest()
+    @CsvSource({
+            "2019, SEPTEMBER, 28",
+            "2019, SEPTEMBER, 29"
+    })
+    void shouldReturnProperDateClassificationForWeekendDays(int year, String month, int day) {
         // Given
-        LocalDate saturdayDate = LocalDate.of(2019, Month.SEPTEMBER, 28);
-        LocalDate sundayDate = LocalDate.of(2019, Month.SEPTEMBER, 29);
+        LocalDate date = LocalDate.of(year, Month.valueOf(month), day);
         // When
-        ReservationDateClassification saturdayDateClassification = ReservationDateClassification.getClassification(saturdayDate);
-        ReservationDateClassification sundayDateClassification = ReservationDateClassification.getClassification(sundayDate);
+        ReservationDateClassification dateClassification = ReservationDateClassification.getClassification(date);
         // Then
-        assertEquals(saturdayDateClassification, ReservationDateClassification.WEEKEND_DAY);
-        assertEquals(sundayDateClassification, ReservationDateClassification.WEEKEND_DAY);
+        assertEquals(ReservationDateClassification.WEEKEND_DAY, dateClassification);
     }
 }
